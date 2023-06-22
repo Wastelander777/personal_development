ac0134621421ea5b7d11130dc5da2212035a608f
Pablo Ugarte <ugarte.p711@gmail.com>
2023-06-22 16:56:14 +0200

    first_step

---
+++ Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+# URL to the problem solved below:
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
