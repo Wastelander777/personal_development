3115a5c0b50cb7ff1231f01f8c4ab4726502e46d
Pablo Ugarte <ugarte.p711@gmail.com>
2023-06-23 10:36:12 +0200

    Working properly

--- Best Time to Buy and Sell Stock with Transaction Fee.py
+++ Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -4,12 +4,26 @@ from typing import List
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
 
 class Solution:
-    def maxProfit(self, prices: List[int], fee: int) -> int:
+
+    # This was my first aproach, but it was not working properly
+    def maxProfitNotWorking(prices: List[int], fee: int) -> int:
         result = []
         bought = False
-        for day_price in range(1,len(prices)):
-            if not bought:
+
+        for day_price in range(0,len(prices)):
+            if not bought and (day_price != len(prices)-1):
                 result.append(prices[day_price])
                 bought = True
-            elif ((result[-1] - day_price) - fee) > 0:
+            elif bought and (abs(result[-1] - prices[day_price]) - fee) > 0:
                 result.append(prices[day_price])
+                bought = False
+
+        return sum([(result[i + 1] - result[i]) - fee for i in range(0, len(result)-1, 2)])
+
+    # This solution works properly and beats 89.61% of users lets gooooo
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        notHold, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            hold = max(hold, notHold - prices[i])
+            notHold = max(notHold, hold + prices[i] - fee)
+        return notHold
